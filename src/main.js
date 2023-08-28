2fde1fe3c4dd72106d37369eca6c944a8860e30d
Muhammad Talha <talhashabbir9@gmail.com>
2023-08-28 05:17:10 +0500

    configurations of store

--- src/main.js
+++ src/main.js
@@ -1,15 +1,29 @@
-import './assets/main.css'
-import "./index.css"
+import "./assets/main.css";
+import "./index.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
+import { useStore } from "./stores/appStore";
+import axios from "axios";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia()).use(router);
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
+
+const store = useStore();
+
+await store.verifyToken();
+
+// Axios configuration
+axios.defaults.headers.common = {
+  Authorization: `Bearer ${store.token}`,
+  Accept: "application/json",
+};
+
+axios.defaults.baseURL = process.env.VUE_APP_BACKEND_BASE_URL;
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
+import { useStore } from '../stores/appStore'
+
+// const store = useStore();
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,7 +12,15 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+
+      beforeEnter: (to) => {
+        const store = useStore()
+        const authenticated = store.authenticated;
+        if ( !authenticated && to.name !== 'login' ) {
+          return { name: 'login' }
+        }
+      }
     },
     {
       path: '/login',
@@ -21,14 +32,6 @@ const router = createRouter({
       name: 'register',
       component: RegisterView
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
   ]
 })
 
