a615f3f37b78a6e14eec3d14751425f945718822
Muhammad Talha <talhashabbir9@gmail.com>
2023-08-30 03:39:19 +0500

    changed name of file and completed the functionality of tasks

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,4 +1,27 @@
+<script setup>
+import { ref, reactive } from 'vue';
+import { useStore } from '../stores/appStore';
+import { useRouter } from 'vue-router';
 
+const email = ref('');
+const password = ref('')
+const store = useStore();
+const router = useRouter();
+
+const loginUser = async() => {
+   await store.loginUser({
+        email: email.value,
+        password: password.value,
+    });
+
+    if(store.token) {
+        router.push('/');
+    } else {
+        alert('Invalid email or password')
+    }
+}
+
+</script>
 
 
 <template>
@@ -13,7 +36,7 @@
           <div>
             <label for="email" class="block text-sm font-medium leading-6 text-white">Email address</label>
             <div class="mt-2">
-              <input id="email" name="email" type="email" autocomplete="email" required="" class="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+              <input id="email" name="email" type="email" autocomplete="email" required="" v-model="email" class="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
             </div>
           </div>
   
@@ -25,12 +48,12 @@
               </div>
             </div>
             <div class="mt-2">
-              <input id="password" name="password" type="password" autocomplete="current-password" required="" class="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+              <input id="password" name="password" type="password" autocomplete="current-password" v-model="password" required="" class="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
             </div>
           </div>
   
           <div>
-            <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+            <button type="button" @click="loginUser" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
           </div>
         </form>
   
